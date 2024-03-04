bbfbb12fc030b5a2f03b0f4047f0122f3895f7dd
Adrien Jouve <adrien.jouve@adn-dev.fr>
2024-03-04 16:35:48 +0100

    Front end date box working again

--- web_pages/js/addUser.js
+++ web_pages/js/addUser.js
@@ -4,87 +4,99 @@ function addUserForm() {
   var form = document.createElement('form');
 
   // Create the label and input elements for FirstName
-  createFirstName(form);
+  var inputFirstName = createFirstName(form);
 
   // Create the label and input elements for LastName
-  createLastName(form);
+  var inputLastName =createLastName(form);
   
   // Create the label and input elements for email
-  createEmail(form);
+  var inputEmail = createEmail(form);
 
   // Create the label element for "Birth Date"
-  createBirthDate(form);
+  var inputBirthDate = createBirthDate(form);
 
   // Create the submit input element
-  var inputSubmit = document.createElement('input');
-  inputSubmit.type = 'submit';
-  inputSubmit.value = 'Submit';
+  var inputSumbit = createSubmit(form);
 
-  var buttonContainer = document.createElement('div');
-  buttonContainer.style.position = 'relative';
-  buttonContainer.appendChild(inputSubmit);
   // Append the buttonContainer to the document body
   document.body.appendChild(form);
-  document.body.appendChild(buttonContainer);
 
-  // Add an event listener to the form to handle the submit event
-  document.addEventListener('submit', function(event) {
-    console.print('IN send data to server');
+
+  // Add an event listener to the inputBirthDate element to open the calendar when clicked
+  document.addEventListener('click', function(event) {
     // Prevent the default form submission behavior
     event.preventDefault();
 
-    // Get the values entered by the user
-    var firstName = inputFirstName.value;
-    var lastName = inputLastName.value;
-    var birthDate = inputBirthDate.value;
-    var email = inputEmail.value;
-
-    // Create a FormData object to store the form data
-    var formData = new FormData();
-    formData.append('FirstName', firstName);
-    formData.append('LastName', lastName);
-    formData.append('BirthDate', birthDate);
-    formData.append('email', email);
-
-    console.log('form information', formData);
-
-    // Create the fetch options
-    var options = {
-      method: 'POST',
-      body: formData,
-    };
-
-    // Log the form data
-    console.log('form information:');
-    for (var pair of formData.entries()) {
-      console.log(pair[0] + ': ' + pair[1]);
+    // Calendar box management
+    if (event.target == inputBirthDate && inputBirthDate.contains(event.target)) {
+      console.log('CLIC on inputBirthDate');
+      // Initialize the Tempus Dominus calendar
+      $(inputBirthDate).datetimepicker('show');
+    }else if (event.target !== inputBirthDate && !inputBirthDate.contains(event.target)) {
+      // Clicked outside of the input element
+      console.log('Clicked outside of the input element');
+      $(inputBirthDate).datetimepicker('hide');
     }
-    
-    // Send the POST request
-    fetch('http://127.0.0.1:5000/api/v1/resources/user', options)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.error('Server responded with error status:', error.response.status);
-          console.error('Response data:', error.response.data);
-        } else if (error.request) {
-            // The request was made but no response was received
-            console.error('No response received from server');
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.error('Error setting up request:', error.message);
+
+    // Submit button management
+    if (event.target == inputSumbit && inputSumbit.contains(event.target)) {
+      console.log('IN send data to server');
+      // Prevent the default form submission behavior
+      event.preventDefault();
+  
+      // Get the values entered by the user
+      var firstName = inputFirstName.value;
+      var lastName = inputLastName.value;
+      var birthDate = inputBirthDate.value;
+      var email = inputEmail.value;
+  
+      // Create a FormData object to store the form data
+      var formData = new FormData();
+      formData.append('FirstName', firstName);
+      formData.append('LastName', lastName);
+      formData.append('BirthDate', birthDate);
+      formData.append('email', email);
+  
+      console.log('form information', formData);
+  
+      // Create the fetch options
+      var options = {
+        method: 'POST',
+        body: formData,
+      };
+  
+      // Log the form data
+      console.log('form information:');
+      for (var pair of formData.entries()) {
+        console.log(pair[0] + ': ' + pair[1]);
       }
-    });
+      
+      // Send the POST request
+      fetch('http://127.0.0.1:5000/api/v1/resources/user', options)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error('Server responded with error status:', error.response.status);
+            console.error('Response data:', error.response.data);
+          } else if (error.request) {
+              // The request was made but no response was received
+              console.error('No response received from server');
+          } else {
+              // Something happened in setting up the request that triggered an Error
+              console.error('Error setting up request:', error.message);
+        }
+      });
+    }
   });
 }
 
@@ -104,6 +116,8 @@ function createFirstName(form) {
   firstNameContainer.appendChild(inputFirstName);
   // Append the firstNameContainer to the document body
   form.appendChild(firstNameContainer);
+
+  return inputFirstName;
 }
 
 function createLastName(form) {
@@ -122,6 +136,8 @@ function createLastName(form) {
   lastNameContainer.appendChild(inputLastName);
   // Append the lastNameContainer to the document body
   form.appendChild(lastNameContainer);
+
+  return inputLastName;
 }
 
 function createEmail(form) {
@@ -139,6 +155,8 @@ function createEmail(form) {
   emailContainer.appendChild(inputEmail);
   // Append the emailContainer to the document body
   form.appendChild(emailContainer);
+
+  return inputEmail;
 }
 
 function createBirthDate(form) {
@@ -160,21 +178,21 @@ function createBirthDate(form) {
   // Append the birthDatecontainer to the document body
   form.appendChild(birthDatecontainer);
 
-  // Add an event listener to the inputBirthDate element to open the calendar when clicked
-  inputBirthDate.addEventListener('click', function() {
-    console.log('CLIC on inputBirthDate');
-    // Initialize the Tempus Dominus calendar
-    $(inputBirthDate).datetimepicker('show');
-  });
-  
-//  document.addEventListener('click', function(event) {
-//  if (event.target !== inputBirthDate && !inputBirthDate.contains(event.target)) {
-//      // Clicked outside of the input element
-//      console.log('Clicked outside of the input element');
-//      $(inputBirthDate).datetimepicker('hide');
-//    }
-//  });
+  return inputBirthDate;
+}
 
+function createSubmit(form) {
+  var inputSubmit = document.createElement('input');
+  inputSubmit.type = 'submit';
+  inputSubmit.value = 'Submit';
+
+  var buttonContainer = document.createElement('div');
+  buttonContainer.style.position = 'relative';
+  buttonContainer.appendChild(inputSubmit);
+
+  document.body.appendChild(buttonContainer);
+
+  return inputSubmit;
 }
 
 $(document).ready(function() {
